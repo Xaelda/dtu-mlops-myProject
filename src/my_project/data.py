e75d9a490cf58479c68a353c79b0b397539c4cf7
Xaelda <92032588+Xaelda@users.noreply.github.com>
2025-01-07 14:01:36 +0100

    Minor update to use paths as Path instead of strings

--- src/my_project/data.py
+++ src/my_project/data.py
@@ -36,16 +36,16 @@ class MyDataset(Dataset):
 
         train_images, train_target = [], []
         for i in range(6):
-            train_images.append(torch.load(f"{self.raw_data_pathH}/train_images_{i}.pt"))
-            train_target.append(torch.load(f"{self.raw_data_path}/train_target_{i}.pt"))
+            train_images.append(torch.load(self.raw_data_path.joinpath("train_images_{i}.pt")))
+            train_target.append(torch.load(self.raw_data_path.joinpath("train_target_{i}.pt")))
 
         # Concatenate the lists to create a single tensor with torch.cat
         train_images = torch.cat(train_images)
         train_target = torch.cat(train_target)
 
         # Indicate expected type of test_images and test_target
-        test_images: torch.Tensor = torch.load(f"{self.raw_data_path}/test_images.pt")
-        test_target: torch.Tensor = torch.load(f"{self.raw_data_path}/test_target.pt")
+        test_images: torch.Tensor = torch.load(self.raw_data_path.joinpath("test_images.pt"))
+        test_target: torch.Tensor = torch.load(self.raw_data_path.joinpath("test_target.pt"))
 
         # Add another dimension to the images tensors
         train_images = train_images.unsqueeze(1).float()
@@ -56,13 +56,13 @@ class MyDataset(Dataset):
         train_images = normalize(train_images)
         test_images = normalize(test_images)
 
-        torch.save(train_images, f"{output_folder}/train_images.pt")
-        torch.save(train_target, f"{output_folder}/train_target.pt")
-        torch.save(test_images, f"{output_folder}/test_images.pt")
-        torch.save(test_target, f"{output_folder}/test_target.pt")
+        torch.save(train_images, output_folder.joinpath("train_images.pt"))
+        torch.save(train_target, output_folder.joinpath("train_target.pt"))
+        torch.save(test_images, output_folder.joinpath("test_images.pt"))
+        torch.save(test_target, output_folder.joinpath("test_target.pt"))
 
 
-def preprocess(raw_data_path: Path, output_folder: Path) -> None:
+def preprocess(raw_data_path: Path = Path("data/raw"), output_folder: Path = Path("data/processed")) -> None:
     print("Preprocessing data...")
     dataset = MyDataset(raw_data_path)
     dataset.preprocess(output_folder)
@@ -71,10 +71,10 @@ def load_processed_data(processed_data_path: Path) -> tuple[torch.utils.data.Dat
     """ Load the preprocessed data and return train and test dataloaders.
         The preprocessed data consist of the corrupted MNIST data.
     """
-    train_images = torch.load(f"{processed_data_path}/train_images.pt")
-    train_target = torch.load(f"{processed_data_path}/train_target.pt")
-    test_images = torch.load(f"{processed_data_path}/test_images.pt")
-    test_target = torch.load(f"{processed_data_path}/test_target.pt")
+    train_images = torch.load(processed_data_path.joinpath("train_images.pt"))
+    train_target = torch.load(processed_data_path.joinpath("train_target.pt"))
+    test_images = torch.load(processed_data_path.joinpath("test_images.pt"))
+    test_target = torch.load(processed_data_path.joinpath("test_target.pt"))
 
     train_set = torch.utils.data.TensorDataset(train_images, train_target)
     test_set = torch.utils.data.TensorDataset(test_images, test_target)
