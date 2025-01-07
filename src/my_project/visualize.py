a1d22cfad00ccd9ad6da95a15187a5d281de7965
Xaelda <92032588+Xaelda@users.noreply.github.com>
2025-01-07 14:30:14 +0100

    Created visualize to make various visualizations of model. Currently implemented PCA and t-SNE visualizations of parameters from last convolutional layer

---
+++ src/my_project/visualize.py
@@ -0,0 +1,61 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import torch
+import typer
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+
+from my_project.models import model
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+MODEL_PATH = Path("models")
+PROCESSED_DATA_PATH = Path("data/processed")
+REPORTS_PATH = Path("reports")
+
+def load_pretrained_model(model_checkpoint: str) -> torch.nn.Module:
+    """Load a trained model."""
+    model = model.to(DEVICE)
+    model.load_state_dict(torch.load(model_checkpoint))
+    return model
+
+def visualize(figure_name: str = "embeddings.png") -> None:
+  """ Visualize model features ... TODO write more details"""
+
+  model = load_pretrained_model(MODEL_PATH.joinpath("model.pth"))
+
+  model.eval()
+  model.fc = torch.nn.Identity()
+
+  test_images = torch.load(PROCESSED_DATA_PATH.joinpath("test_images.pt"))
+  test_targets = torch.load(PROCESSED_DATA_PATH.joinpath("test_target.pt"))
+  test_dataset = torch.utils.data.TensorDataset(test_images, test_targets)
+
+  embeddings, targets = [], []
+  with torch.inference_mode():
+    for batch in torch.utils.data.DataLoader(test_dataset, batch_size=32):
+      images, targets = batch
+      predictions = model(images)
+      embeddings.append(predictions.to(DEVICE))
+      targets.append(targets)
+    embeddings = torch.cat(embeddings).numpy()
+    targets = torch.cat(targets).numpy()
+
+  if embeddings.shape[1] > 500: # Reduce dimensionality for large embeddings
+        pca = PCA(n_components=100)
+        embeddings = pca.fit_transform(embeddings)
+  tsne = TSNE(n_components=2)
+  embeddings = tsne.fit_transform(embeddings)
+
+  plt.figure(figsize=(10, 10))
+  for i in range(10):
+      mask = targets == i
+      plt.scatter(embeddings[mask, 0], embeddings[mask, 1], label=str(i))
+  plt.legend()
+  plt.savefig(REPORTS_PATH.joinpath("figures", figure_name))
+
+if __name__ == "__main__":
+    typer.run(visualize)
+
+
